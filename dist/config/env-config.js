8d13d426124d94bd1f3065c6412becd2b1203d75
book1317 <bookboks@hotmail.com>
2023-04-28 01:28:21 +0700

    update dist

--- dist/config/env-config.js
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-window._env_ = {
-  REACT_APP_MOVIE_BASE_URL: "https://api.themoviedb.org/3",
-  REACT_APP_API_KEY: "b04aa62b7974a42d03f5aea0a54faa0d",
-  REACT_APP_IMAGE_BASE_URL: "https://image.tmdb.org/t/p/w500",
-};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://book1317.github.io/react-front-end-every-matrix/",
   plugins: [react()],
 });
